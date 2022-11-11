ff6d1d32c366c52fc61ce37a82fa5560f69eeff9
pranavpv773 <pranavpv773@gmail.com>
2022-11-11 14:36:38 +0530

    all the bottom screens added

---
+++ lib/app/core/app_colors/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static Color kPrimary = const Color(0xff221E1E);
+  static Color kSecondary = const Color(0xffdb0000);
+}
--- lib/app/features/me/view/me.dart
+++ lib/app/features/me/view/me.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class GlobalScreen extends StatelessWidget {
-  const GlobalScreen({Key? key}) : super(key: key);
+class MeScreen extends StatelessWidget {
+  const MeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
